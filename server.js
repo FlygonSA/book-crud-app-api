8c31b66dc78d4a6398265f798c594e6506735119
FlygonSA <frastacho2002@gmail.com>
2021-01-29 20:44:02 -0300

    Switched api calls to Async functions

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const BookSchema = require('./model/item');
+const bookSchema = require('./api/books/item');
 
 const app = express();
 
@@ -26,37 +26,63 @@ app.use(express.json());
 // Middleware
 
 // Get books from the database
-app.get('/api/books', (req, res) => {
-  BookSchema.find().then((books) => res.json(books));
+app.get('/api/books', async (req, res) => {
+  try {
+    const books = await bookSchema.find();
+    res.json(books);
+  } catch (error) {
+    res
+      .status(404)
+      .json({ message: 'No books were found' });
+  }
 });
 
 // Add a new book to the database
-app.post('/api/books/create', (req, res) => {
-  const newBook = new BookSchema(req.body);
-  newBook.save()
-    .then(() => { res.json('Book saved succesfully'); })
-    .catch(() => { res.json('There was an error, try again'); });
+app.post('/api/books/create', async (req, res) => {
+  const newBook = new bookSchema(req.body);
+  try {
+    await newBook.save();
+    res.json({ message: 'Book created sucessfully' });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: 'There was an error, try again' });
+  }
 });
 
 // Delete book from database
-app.post('/api/books/delete', (req, res) => {
-  if (BookSchema.exists({ id: req.body.id })) {
-    BookSchema.findByIdAndDelete(req.body.id)
-      .then(() => { res.json('Book deleted succesfully'); })
-      .catch(() => { res.json('There was an error, try again'); });
+app.post('/api/books/delete', async (req, res) => {
+  if (bookSchema.exists({ id: req.body.id })) {
+    try {
+      await bookSchema.findByIdAndDelete(req.body.id);
+      res.json({ message: 'Book sucessfully deleted' });
+    } catch (error) {
+      res
+        .status(400)
+        .json({ message: 'There was an error, please try again' });
+    }
   } else {
-    res.status(404).json('Book missing from database');
+    res
+      .status(404)
+      .json('Book missing from database');
   }
 });
 
 // Modify book from database
-app.post('/api/books/modify', (req, res) => {
+app.post('/api/books/modify', async (req, res) => {
   // Gets the ID, and modifies the collection acording to the data you send on the rest of the JSON
-  if (BookSchema.exists({ id: req.body.id })) {
-    BookSchema.findByIdAndUpdate(req.body.id, req.body)
-      .then(() => { res.json('Book modified succesfully'); })
-      .catch(() => { res.json('There was an error, try again'); });
+  if (bookSchema.exists({ id: req.body.id })) {
+    try {
+      await bookSchema.findByIdAndUpdate(req.body.id, req.body);
+      res.json({ message: 'Book sucessfully modified' });
+    } catch (error) {
+      res
+        .status(400)
+        .json({ message: 'There was an error, please try again' });
+    }
   } else {
-    res.status(404).json('Book missing from database');
+    res
+      .status(404)
+      .json('Book missing from database');
   }
 });
